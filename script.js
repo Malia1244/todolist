ec731669d6c28488890b27ba16840fa59a3ceea7
Malia1244 <malsia.luvly@gmail.com>
2025-07-22 15:24:35 -0600

    Add task

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello from script.js!");
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("addButton");
+  const taskList = document.getElementById("taskList");
+  const words = document.getElementById("taskInput");
+
+  button.addEventListener("click", function() {
+    alert(words.value);
+
+const newItem = document.createElement("li");
+newItem.textContent = words.value;
+taskList.appendChild(newItem);
+words.value = ""; 
+  });
+});
